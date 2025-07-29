b18b19bd67af072e680478bb1b16582869086a14
annlee-dd <ann.lee@datadoghq.com>
2025-07-29 16:24:12 +1000

    refactor: remove redundant Datadog initialisation

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
-// DD Agent App level config
-const tracer = require('dd-trace').init({
-  service: process.env.DD_SERVICE || 'flowise-langchain',
-  env: process.env.DD_ENV || 'dev',
-  version: process.env.DD_VERSION || '1.0.0',
-  logInjection: true,
-  llmObservability: true,
-  integrations: {
-    langchain: true,
-    openai: true,
-    anthropic: true,
-    bedrock: true,
-    vertexai: true
-  }
-});
-
-console.log('Datadog enabled for Flowise');
-
 const { startFlowise } = require('flowise');
 startFlowise();
